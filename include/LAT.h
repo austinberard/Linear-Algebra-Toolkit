1b2cc54706ec0e5889c9355a5942d32aeb61824a
Austin Berard <austinberard@outlook.com>
2016-11-18 15:28:02 -0500

    Contains the operations

--- include/LAT.h
+++ include/LAT.h
@@ -5,12 +5,15 @@
 #ifndef LINEAR_ALGEBRA_TOOLKIT_LAT_H
 #define LINEAR_ALGEBRA_TOOLKIT_LAT_H
 
-
-
-class LAT {
-
-};
-
+#include "Vector.h"
+typedef double scalar;
+namespace LAT{
+    Vector multiply(Vector &vectorX, Vector &vectorY);
+    Vector multiply(scalar c, Vector &vectorY);
+    Vector add(Vector &vectorX, Vector &vectorY);
+    Vector cross(Vector &vectorX, Vector &vectorY);
+    bool equal(Vector &vectorX, Vector &vectorY);
+}
 
 
 #endif //LINEAR_ALGEBRA_TOOLKIT_LAT_H
--- src/LAT.cpp
+++ src/LAT.cpp
@@ -2,4 +2,36 @@
 // Created by Austin Berard on 11/18/16.
 //
 
-#include "LAT.h"
+#include "../include/LAT.h"
+
+typedef double scalar;
+namespace LAT{
+
+    Vector multiply(Vector &vectorX, Vector &vectorY) {
+        return Vector(0);
+    }
+
+    Vector multiply(scalar c, Vector &vectorY) {
+        return Vector(0);
+    }
+
+    Vector add(Vector &vectorX, Vector &vectorY) {
+        if(vectorX.getSize() != vectorY.getSize()){
+            throw "Can not add vectors of different sizes";
+        }
+        Vector tmp(vectorX.getSize());
+        for(int i = 0; i < vectorX.getSize(); i++){
+            double cur = vectorX.get(i) + vectorY.get(i);
+            tmp.set(i, cur);
+        }
+        return tmp;
+    }
+
+    Vector cross(Vector &vectorX, Vector &vectorY) {
+        return Vector(0);
+    }
+
+    bool equal(Vector &vectorX, Vector &vectorY) {
+        return false;
+    }
+}
