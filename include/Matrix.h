23348f8127f77a5287c85b0f02a40216769eaaca
Austin Berard <austinberard@outlook.com>
2016-11-20 03:04:19 -0500

    Added Matrix class, and defined some functions

--- include/Matrix.h
+++ include/Matrix.h
@@ -5,12 +5,33 @@
 #ifndef LINEAR_ALGEBRA_TOOLKIT_MATRIX_H
 #define LINEAR_ALGEBRA_TOOLKIT_MATRIX_H
 
+#include <vector>
+#include "Vector.h"
 
+typedef std::vector<double> vector_d;
+typedef std::vector<std::vector<double>> vector_vector_d;
+typedef std::vector<Vector> vector_V;
 
 class Matrix {
+private:
+    int m;
+    int n;
+    vector_vector_d elements;
+public:
+    Matrix(int m, int n);
+    Matrix(int n);
+    Matrix(vector_vector_d elems);
+    Matrix(vector_V);
+    int getM() const { return m;}
+    int getN() const { return n;}
+    double get(int m, int n) const {return elements[m][n];}
+    void set(int m, int n, double val);
+    void setRow(int m, Vector vec);
+    void setCol(int n, Vector vec);
+    void print() const;
+    bool equals(Matrix &matrixY);
 
 };
-
 
 
 #endif //LINEAR_ALGEBRA_TOOLKIT_MATRIX_H
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -2,4 +2,4 @@
 // Created by Austin Berard on 11/20/16.
 //
 
-#include "Matrix.h"
+#include "../include/Matrix.h"
