824eebbb14cb38867ab7a04722f818c7eb3c0eaf
Austin Berard <austinberard@outlook.com>
2016-11-22 18:52:51 -0500

    Have to implement functions still, did one constructor

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "../include/Matrix.h"
+using namespace std;
+Matrix::Matrix(int m, int n) {
+    rows = m;
+    cols = n;
+    elements = *(new std::vector<std::vector<double>>);
+
+    elements.resize(rows);
+    for(int i = 0 ; i < rows ; ++i){
+        elements[i].resize(cols);
+    }
+}
+
+Matrix::Matrix(int n) {
+    rows = n;
+    cols = n;
+    elements = *(new std::vector<std::vector<double>>);
+    
+    elements.resize(rows);
+    for(int i = 0 ; i < rows ; ++i){
+        elements[i].resize(cols);
+    }
+
+}
+
+Matrix::Matrix(vector_vector_d elems) {
+
+}
+
+Matrix::Matrix(vector_V) {
+
+}
+
+void Matrix::set(int m, int n, double val) {
+
+}
+
+void Matrix::setRow(int m, Vector vec) {
+
+}
+
+void Matrix::setCol(int n, Vector vec) {
+
+}
+
+void Matrix::print() const {
+
+}
+
+bool Matrix::equals(Matrix &matrixY) {
+    return false;
+}
