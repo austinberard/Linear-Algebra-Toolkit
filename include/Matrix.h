589dec70781de8a03f71851c747a06fad18a0913
Austin Berard <austinberard@outlook.com>
2016-11-22 18:52:12 -0500

    Changed instance var names

--- include/Matrix.h
+++ include/Matrix.h
@@ -14,23 +14,22 @@ typedef std::vector<Vector> vector_V;
 
 class Matrix {
 private:
-    int m;
-    int n;
+    int rows;
+    int cols;
     vector_vector_d elements;
 public:
     Matrix(int m, int n);
     Matrix(int n);
     Matrix(vector_vector_d elems);
     Matrix(vector_V);
-    int getM() const { return m;}
-    int getN() const { return n;}
+    int getM() const { return rows;}
+    int getN() const { return cols;}
     double get(int m, int n) const {return elements[m][n];}
     void set(int m, int n, double val);
     void setRow(int m, Vector vec);
     void setCol(int n, Vector vec);
     void print() const;
     bool equals(Matrix &matrixY);
-
 };
 
 
